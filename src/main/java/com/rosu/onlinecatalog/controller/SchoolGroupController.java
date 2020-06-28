ccd8308962ec2c0ab9f1dafdf7df94862a4cab2d
rflorian2017 <roby_florian@yahoo.com>
2020-06-28 12:17:13 +0300

    updates for school group

---
+++ src/main/java/com/rosu/onlinecatalog/controller/SchoolGroupController.java
@@ -0,0 +1,45 @@
+package com.rosu.onlinecatalog.controller;
+
+import com.rosu.onlinecatalog.model.SchoolGroup;
+import com.rosu.onlinecatalog.model.Student;
+import com.rosu.onlinecatalog.service.SchoolGroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class SchoolGroupController {
+
+    @Autowired
+    private SchoolGroupService schoolGroupService;
+
+    @GetMapping("allschoolgroups")
+    public String showAllGroups(Model model) {
+        List<SchoolGroup> schoolGroups = schoolGroupService.findAll();
+
+        model.addAttribute("schoolgroups", schoolGroups);
+
+        return "/schoolgroup/showallschoolgroups";
+     }
+
+    @GetMapping("/addschoolgroup")
+    public String addSchoolGroup(Model model) {
+        model.addAttribute("schoolgroup", new SchoolGroup()); // initial bind with the form, to say to the webpage
+        // what is the type of student th:object
+
+        return "schoolgroup/addschoolgroup";
+    }
+
+    @PostMapping("/addschoolgroup")
+    public String addSchoolGroup(@ModelAttribute SchoolGroup schoolGroup) {
+//        System.out.println(student);
+        schoolGroupService.save(schoolGroup);
+        return "redirect:/allschoolgroups";
+        //TODO: show in same page on the left all students, on the right add a new student
+    }
+}
---
+++ src/main/java/com/rosu/onlinecatalog/service/SchoolGroupService.java
@@ -0,0 +1,27 @@
+package com.rosu.onlinecatalog.service;
+
+import com.rosu.onlinecatalog.model.SchoolGroup;
+import com.rosu.onlinecatalog.model.Student;
+import com.rosu.onlinecatalog.repository.SchoolGroupRepository;
+import com.rosu.onlinecatalog.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service("ISchoolGroupService")
+public class SchoolGroupService {
+
+    @Autowired
+    private SchoolGroupRepository schoolGroupRepository;
+
+
+    public List<SchoolGroup> findAll() {
+        return schoolGroupRepository.findAll();
+    }
+
+    public void save(SchoolGroup schoolGroup) {
+        schoolGroupRepository.save(schoolGroup);
+    }
+}
