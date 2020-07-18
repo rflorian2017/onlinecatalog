1a506dda37a3288ad84515db18f4ddd185bed7db
rflorian2017 <roby_florian@yahoo.com>
2020-07-18 12:57:45 +0300

    rest controller

---
+++ src/main/java/com/rosu/onlinecatalog/controller/rest/StudentRestController.java
@@ -0,0 +1,24 @@
+package com.rosu.onlinecatalog.controller.rest;
+
+import com.rosu.onlinecatalog.model.Student;
+import com.rosu.onlinecatalog.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping("/students")
+    public ResponseEntity<List<Student>> getAllStudents() {
+        return ResponseEntity.ok(studentRepository.findAll());
+    }
+}
